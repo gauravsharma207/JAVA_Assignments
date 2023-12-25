4d74035cc56172d74749736ad5a1e61594f11dca
Gaurav <gsharma207@gmail.com>
2023-12-26 00:19:24 +0530

    changes in all loops

--- src/assignments/AllLoops.java
+++ src/assignments/AllLoops.java
@@ -3,7 +3,8 @@ package assignments;
 public class AllLoops {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("All Loops");
+		System.out.println("All Loops");
 
 	}
 
